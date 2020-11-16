60d43c9c3570d8d51d28dd46742827c91ecd599d
Miguel Caringal <Miguel.Caringal@gmail.com>
2020-11-15 22:23:39 -0500

    Initial Board

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Toast from 'react-bootstrap/Toast';
+import Container from 'react-bootstrap/Container';
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Button';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+/*
+class Table extends React.Component{
+  render () {
+    <Container>
+      <Row>
+        <Col>
+          <svg width={400} height={180}>
+      <     rect x={50} y={20} rx={20} ry={20} width={150} height={150} style={{fill: 'red', stroke: 'black', strokeWidth: 5, opacity: '0.5'}} />
+          </svg>
+        </Col>
+      </Row>
+    </Container>
+  }
+};
+*/
+
+
+const App = () => (
+  <Container>
+    <Row class = "board-container">
+        <div class = "board">
+          <svg class = "board-svg">
+            <rect class = "board-rect" rx={100} ry={100} style={{fill: 'red', stroke: 'black', strokeWidth: 5, opacity: '0.5'}} />
+          </svg>
+        </div>
+    </Row>
+  </Container>
+);
+
+
 
 export default App;
+
+
+/*
+<svg width={400} height={180}>
+  <rect x={50} y={20} rx={20} ry={20} width={150} height={150} style={{fill: 'red', stroke: 'black', strokeWidth: 5, opacity: '0.5'}} />
+</svg>
+
+const App = () => (
+  <Container fluid>
+    <Row class = "board-container">
+        <Col class = "board-c">
+          <svg class="board-svg">
+            <rect class = "board" rx={100} ry={100} style={{fill: 'red', stroke: 'black', strokeWidth: 5, opacity: '0.5'}} />
+          </svg>
+        </Col>
+    </Row>
+  </Container>
+);
+*/
--- src/index.js
+++ src/index.js
@@ -4,13 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-
-
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
