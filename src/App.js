1ec74171163f20b46483fa7a09689785383f70ce
Miguel Caringal <Miguel.Caringal@gmail.com>
2020-11-17 15:59:28 -0500

    somewhat working table with filler

--- src/App.js
+++ src/App.js
@@ -1,64 +1,57 @@
 import React, { useState } from 'react';
 
-
-import Jumbotron from 'react-bootstrap/Jumbotron';
-import Toast from 'react-bootstrap/Toast';
 import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col'
 import Row from 'react-bootstrap/Row'
-import Button from 'react-bootstrap/Button';
+import Table from 'react-bootstrap/Table';
 
 import './App.css';
 
-
-/*
-class Table extends React.Component{
-  render () {
+const App = () => (
+  <div class = "overall">
     <Container>
-      <Row>
-        <Col>
-          <svg width={400} height={180}>
-      <     rect x={50} y={20} rx={20} ry={20} width={150} height={150} style={{fill: 'red', stroke: 'black', strokeWidth: 5, opacity: '0.5'}} />
-          </svg>
-        </Col>
+      <Row class = "board-container">
+          <div class = "board">
+            <Table id = "board-table">
+              <thead>
+                <tr class = "board-table-row">
+                  <th>#</th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>Username</th>
+                  <th>Username</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr class = "board-table-row">
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>
+                    <Table>
+                      <tr class = "board-table-row">
+                        <td>a</td>
+                        <td>b</td>
+                        <td>c</td>
+                        <td>d</td>
+                        <td>e</td>
+                      </tr>
+                    </Table>
+                  </td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                </tr>
+                <tr class = "board-table-row">
+                  <td>1</td>
+                  <td>Mark</td>
+                  <td>Otto</td>
+                  <td>@mdo</td>
+                  <td>@mdo</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
       </Row>
     </Container>
-  }
-};
-*/
-
-
-const App = () => (
-  <Container>
-    <Row class = "board-container">
-        <div class = "board">
-          <svg class = "board-svg">
-            <rect class = "board-rect" rx={100} ry={100} style={{fill: 'red', stroke: 'black', strokeWidth: 5, opacity: '0.5'}} />
-          </svg>
-        </div>
-    </Row>
-  </Container>
+  </div>
 );
 
-
-
-export default App;
-
-
-/*
-<svg width={400} height={180}>
-  <rect x={50} y={20} rx={20} ry={20} width={150} height={150} style={{fill: 'red', stroke: 'black', strokeWidth: 5, opacity: '0.5'}} />
-</svg>
-
-const App = () => (
-  <Container fluid>
-    <Row class = "board-container">
-        <Col class = "board-c">
-          <svg class="board-svg">
-            <rect class = "board" rx={100} ry={100} style={{fill: 'red', stroke: 'black', strokeWidth: 5, opacity: '0.5'}} />
-          </svg>
-        </Col>
-    </Row>
-  </Container>
-);
-*/
+export default App; 
