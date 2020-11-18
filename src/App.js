f72d30ecebe36e25ea234944704f1e1603fd3deb
Miguel-Caringal <Miguel.Caringal@gmail.com>
2020-11-17 23:21:02 -0500

    Redo table

--- src/App.js
+++ src/App.js
@@ -2,64 +2,41 @@ import React, { useState } from 'react';
 
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Row'
 import Table from 'react-bootstrap/Table';
 
 import './App.css';
 
+// we are gaming
+
 const App = () => (
   <div class = "overall">
     <Container>
-      <Row class = "board-container">
-          <div class = "board">
-            <Table id = "board-table">
-              <thead>
-                <tr class = "board-table-row">
-                  <th></th>
-                  <th>Player 1</th>
-                  <th>Player 2</th>
-                  <th>Player 3</th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr class = "board-table-row">
-                  <td>Player 4</td>
-                  <td></td>
-                  <td>
-                    <Table>
-                      <tr class = "board-table-row">
-                        <td>
-                          <img src = "./cards/CLUB-1.svg" class = "board-card"></img>
-                        </td>
-                        <td>
-                          <img src = "./cards/HEART-11-JACK.svg" class = "board-card"></img>
-                        </td>
-                        <td>
-                          <img src = "./cards/CLUB-3.svg" class = "board-card"></img>
-                        </td>
-                        <td>
-                          <img src = "./cards/CLUB-4.svg" class = "board-card"></img>
-                        </td>
-                        <td>
-                          <img src = "./cards/CLUB-5.svg" class = "board-card"></img>
-                        </td>
-                      </tr>
-                    </Table>
-                  </td>
-                  <td></td>
-                  <td>Player 8</td>
-                </tr>
-                <tr class = "board-table-row">
-                  <td></td>
-                  <td>Player 5</td>
-                  <td>Player 6</td>
-                  <td>Player 7</td>
-                  <td></td>
-                </tr>
-              </tbody>
-            </Table>
-          </div>
+      <Row class = "board-container bg-info" style={{height:"25vh"}}>
+          <div class="col-10 m-auto bg-success"></div>
       </Row>
+      <Row class = "board-container m-auto bg-white" style={{height:"40vh"}}>
+          <div class="col-10 m-auto table" style={{height:"100%"}}>
+        a
+          </div>
+          {/* You */}
+          <div class="player bg-info" style={{left: "50%", top: "78%"}}></div>
+          <div class="player bg-info" style={{left: "32%", top: "78%"}}></div>
+          <div class="player bg-info" style={{left: "68%", top: "78%"}}></div>
+
+          {/* right side */}
+          <div class="player bg-info" style={{left: "80%", top: "56%"}}></div>
+          <div class="player bg-info" style={{left: "80%", top: "30%"}}></div>
+
+          {/* left side */}
+          <div class="player bg-info" style={{left: "20%", top: "56%"}}></div>
+          <div class="player bg-info" style={{left: "20%", top: "30%"}}></div>
+
+          {/* Top */}
+          <div class="player bg-info" style={{left: "62%", top: "12%"}}></div>
+          <div class="player bg-info" style={{left: "38%", top: "12%"}}></div>
+      </Row>
+      <Row class = "board-container m-auto bg-white" style={{height:"35vh"}}></Row>
     </Container>
   </div>
 );
