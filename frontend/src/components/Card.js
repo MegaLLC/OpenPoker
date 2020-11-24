0aa297f3d2cd0554935ad1510bd2d5b961ee22b0
Howard Yun <haoy2001@gmail.com>
2020-11-24 01:03:52 -0500

    Card resizing

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Card.css";
 
+// card ratio is 1.4
+// ex: 239w x 335h
+
 export class Card extends React.Component {
   render() {
     return (
-      <div className="card-wrapper">
-        <div className="card bg-secondary mx-1">
+      <div className="card-wrapper m-auto">
+        <div className="card bg-secondary">
           <img src={"./cards/hd.svg"} className="card-image card-back" alt=""></img>
           <img src={"./cards/" + this.props.card + ".svg"} className="card-image card-front" alt=""></img>
         </div>
