446f4bfe3694ad6532a579e69437bc2f71aed744
Howard Yun <haoy2001@gmail.com>
2020-12-28 07:29:33 -0500

    Fix issues with cards

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -1,19 +1,14 @@
 import React from "react";
-import "./Card.css";
 
 // card ratio is 1.4
 // ex: 239w x 335h
 
 const Card = (props) => {
-  return (
-    <div className="card-wrapper m-auto">
-      {props.card && props.card !== "EM" && (
-        <div className="card bg-secondary">
-          <img src={"./cards/" + props.card + ".svg"} className="card-image card-front" alt=""></img>
-        </div>
-      )}
-    </div>
-  );
+  if (props.card && props.card !== "EM") {
+    return <img src={"./cards/" + props.card + ".svg"} alt="" className={"h-100 p-2"}></img>;
+  } else {
+    return <div></div>;
+  }
 };
 
 export default Card;
